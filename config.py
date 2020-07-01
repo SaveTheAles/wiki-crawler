57044d7053c0ec2c69481a3e37debbdc15d3e434
Ales Puchilo <ales.puchilo@gmail.com>
2020-07-01 18:01:05 +0300

    upd config.py

--- config.py
+++ config.py
@@ -7,14 +7,11 @@ WIKI_IPFS = 'QmXoypizjW3WknFiJnKLwHCnL72vedxjQkDDP1mXWo6uco'
 # path to links.csv as db of cyberlinks broadcasted. str
 PATH_TO_DF = './data/links.csv'
 
-# cyber RPC API adress. str 
+# cyber RPC API adress ('http://address:port'). str
 RPC_API = 
 
-# cyber LCD API adress. str 
-LCD_API = 
-
-# cyber proxy node for broadcasting transactions. str
-PROXY_NODE = 
+# cyber LCD API adress ('http://address:port'). str
+LCD_API =
 
 # the max amount of cyberlinks in one transaction. int
 CHUNK = 500
